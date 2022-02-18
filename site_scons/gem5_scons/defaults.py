feb382f87561695093d3da8428bd1647edbf27d0
Gabe Black <gabe.black@gmail.com>
2022-02-18 00:51:57 -0800

    scons: Pass the DISPLAY environment variable through to SCons.
    This lets gui programs run correctly within SCons, specifically the
    kconfig "guiconfig" helper utility.
    
    Change-Id: Iec51df3db89ac7e7411e6c08fe8201afb69dc63e
    Reviewed-on: https://soc-sim-external-review.googlesource.com/c/gem5/gem5/+/7319
    Tested-by: kokoro <noreply+kokoro@google.com>
    Presubmit-Ready: Gabe Black <gabeblack@google.com>
    Reviewed-by: Yu-hsin Wang <yuhsingw@google.com>

--- site_scons/gem5_scons/defaults.py
+++ site_scons/gem5_scons/defaults.py
@@ -48,7 +48,8 @@ def EnvDefaults(env):
                      'LIBRARY_PATH', 'PATH', 'PKG_CONFIG_PATH', 'PROTOC',
                      'PYTHONPATH', 'RANLIB', 'TERM', 'PYTHON_CONFIG',
                      'CCFLAGS_EXTRA', 'GEM5PY_CCFLAGS_EXTRA',
-                     'GEM5PY_LINKFLAGS_EXTRA', 'LINKFLAGS_EXTRA'])
+                     'GEM5PY_LINKFLAGS_EXTRA', 'LINKFLAGS_EXTRA',
+                     'DISPLAY'])
 
     use_prefixes = [
         "ASAN_",           # address sanitizer symbolizer path and settings
