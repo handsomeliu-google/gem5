e9c85fe34ecbb49da989b0f205b1c682532bded0
Gabe Black <gabe.black@gmail.com>
2022-02-24 02:15:52 -0800

    cpu,sim: Don't tie ThreadContext contextId to the CPU ID.
    The contextId is generally treated as (and should be) an opaque index
    into the System objects threadContext array. When forcing it to
    particular values, that introduces gaps in the threadContext array which
    trips up other code which is expecting the array to have only valid
    entries.
    
    Change-Id: I4997e989b436a3008f65f348722dfb843b2f110a
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/57089
    Maintainer: Gabe Black <gabe.black@gmail.com>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Yu-hsin Wang <yuhsingw@google.com>
    Reviewed-on: https://soc-sim-external-review.googlesource.com/c/gem5/gem5/+/6952
    Reviewed-by: Gabe Black <gabeblack@google.com>
    Presubmit-Ready: Gabe Black <gabeblack@google.com>
    Tested-by: Yu-hsin Wang <yuhsingw@google.com>

--- src/sim/system.hh
+++ src/sim/system.hh
@@ -143,7 +143,7 @@ class System : public SimObject, public PCEventScope
             return threads[id];
         }
 
-        void insert(ThreadContext *tc, ContextID id=InvalidContextID);
+        void insert(ThreadContext *tc);
         void replace(ThreadContext *tc, ContextID id);
 
         friend class System;
@@ -574,8 +574,7 @@ class System : public SimObject, public PCEventScope
 
   public:
 
-    void registerThreadContext(
-            ThreadContext *tc, ContextID assigned=InvalidContextID);
+    void registerThreadContext(ThreadContext *tc);
     void replaceThreadContext(ThreadContext *tc, ContextID context_id);
 
     void serialize(CheckpointOut &cp) const override;
