e56d7f418ab5961172992294ffd274db06ab6e6b
Earl Ou <shunhsingou@google.com>
2022-06-27 08:31:57 +0000

    dev: avoid intpin to reset value at binding stage
    By design SimObject should initialize its state at init() stage.
    However, the original intpin design will try to reset the sink side when
    binding. This could cause unexpected issue as the other side does not
    init() yet.
    
    To align with the design, the call to upper()/lower() should be left to
    the initiator in the init() function instead of constructor.
    
    Bug: 237198873
    Test: build and run gem5
    Change-Id: Iec8b228715d093381a33e747849119562bd634e1
    Reviewed-on: https://soc-sim-external-review.googlesource.com/c/gem5/gem5/+/7835
    Presubmit-Ready: Earl Ou <shunhsingou@google.com>
    Reviewed-by: Yu-hsin Wang <yuhsingw@google.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- src/dev/intpin.hh
+++ src/dev/intpin.hh
@@ -97,8 +97,8 @@ class IntSourcePinBase : public Port
     bool _state = false;
 
   public:
-    IntSourcePinBase(const std::string &_name, PortID _id, bool def_state) :
-        Port(_name, _id), _state(def_state)
+    IntSourcePinBase(const std::string &_name, PortID _id):
+        Port(_name, _id)
     {}
 
     void raise() { sink->raise(); }
@@ -112,9 +112,8 @@ template <class Device>
 class IntSourcePin : public IntSourcePinBase
 {
   public:
-    IntSourcePin(const std::string &_name, PortID _id, Device *owner,
-                 bool def_state=false) :
-        IntSourcePinBase(_name, _id, def_state)
+    IntSourcePin(const std::string &_name, PortID _id, Device *owner) :
+        IntSourcePinBase(_name, _id)
     {}
 };
 
