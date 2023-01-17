0bf5196f29a10d37c75af2fc5f34af5a59959070
Xuan Hu <huxuan@bosc.ac.cn>
2023-01-17 21:08:57 +0800

    arch-riscv,cpu-minor: Add MinorDefaultVecFU for risc-v v-ext
    Change-Id: Id5c5ae5fa1901154cadeb0a4958703f3f15d491f
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/67295
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Maintainer: Jason Lowe-Power <power.jg@gmail.com>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-on: https://soc-sim-external-review.googlesource.com/c/gem5/gem5/+/11600
    Reviewed-by: Roger Chang <rogerycchang@google.com>
    Gem5-Virtual-Platform-Presubmit-Ready: Roger Chang <rogerycchang@google.com>
    Presubmit-Ready: Roger Chang <rogerycchang@google.com>

--- src/cpu/minor/BaseMinorCPU.py
+++ src/cpu/minor/BaseMinorCPU.py
@@ -245,6 +245,33 @@ class MinorDefaultMiscFU(MinorFU):
     opLat = 1
 
 
+class MinorDefaultVecFU(MinorFU):
+    opClasses = minorMakeOpClassSet(
+        [
+            "VectorUnitStrideLoad",
+            "VectorUnitStrideStore",
+            "VectorUnitStrideMaskLoad",
+            "VectorUnitStrideMaskStore",
+            "VectorStridedLoad",
+            "VectorStridedStore",
+            "VectorIndexedLoad",
+            "VectorIndexedStore",
+            "VectorUnitStrideFaultOnlyFirstLoad",
+            "VectorWholeRegisterLoad",
+            "VectorWholeRegisterStore",
+            "VectorIntegerArith",
+            "VectorFloatArith",
+            "VectorFloatConvert",
+            "VectorIntegerReduce",
+            "VectorFloatReduce",
+            "VectorMisc",
+            "VectorIntegerExtension",
+            "VectorConfig",
+        ]
+    )
+    opLat = 1
+
+
 class MinorDefaultFUPool(MinorFUPool):
     funcUnits = [
         MinorDefaultIntFU(),
@@ -255,6 +282,7 @@ class MinorDefaultFUPool(MinorFUPool):
         MinorDefaultPredFU(),
         MinorDefaultMemFU(),
         MinorDefaultMiscFU(),
+        MinorDefaultVecFU(),
     ]
 
 
