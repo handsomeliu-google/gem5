281104ce71e7d72563d7848480bd91ae25165dcf
Gabriel Busnot <gabriel.busnot@arteris.com>
2023-02-02 16:16:24 +0000

    sim: Switch from EventWrapper to MemberEventWrapper before deprec
    Change-Id: I25c81787d522a0dd063112b6727669da46e0f0e7
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/67655
    Reviewed-by: Daniel Carvalho <odanrc@yahoo.com.br>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Maintainer: Jason Lowe-Power <power.jg@gmail.com>
    Reviewed-on: https://soc-sim-external-review.googlesource.com/c/gem5/gem5/+/11951
    Presubmit-Ready: Roger Chang <rogerycchang@google.com>
    Reviewed-by: Roger Chang <rogerycchang@google.com>

--- src/base/remote_gdb.hh
+++ src/base/remote_gdb.hh
@@ -273,8 +273,8 @@ class BaseRemoteGDB
 
     BaseGdbRegCache *regCachePtr = nullptr;
 
-    EventWrapper<BaseRemoteGDB, &BaseRemoteGDB::connect> connectEvent;
-    EventWrapper<BaseRemoteGDB, &BaseRemoteGDB::detach>  disconnectEvent;
+    MemberEventWrapper<&BaseRemoteGDB::connect> connectEvent;
+    MemberEventWrapper<&BaseRemoteGDB::detach>  disconnectEvent;
 
     class TrapEvent : public Event
     {
@@ -307,7 +307,7 @@ class BaseRemoteGDB
 
     // Single step.
     void singleStep();
-    EventWrapper<BaseRemoteGDB, &BaseRemoteGDB::singleStep> singleStepEvent;
+    MemberEventWrapper<&BaseRemoteGDB::singleStep> singleStepEvent;
 
     void clearSingleStep();
     void setSingleStep();
