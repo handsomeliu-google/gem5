86347567adfa27e257729cc406e2f3b6b9472342
Roger Chang <rogerycchang@google.com>
2024-01-25 04:44:33 +0000

    Revert "arch-riscv, not-for-upstream: Set NumInterruptTypes number to 256"
    This reverts commit 960b70d1486db5aacc6b4f2dc922d0c78e321f18.
    
    Reason for revert: The CLIC will not be used in our MBU SC
    
    Change-Id: I7740698d3331b3a99ddd19442b5a35d979d96e63
    Reviewed-on: https://soc-sim-external-review.googlesource.com/c/gem5/gem5/+/14801
    Perf-Presubmit-Ready: Roger Chang <rogerycchang@google.com>
    Reviewed-by: Yu-hsin Wang <yuhsingw@google.com>
    Gem5-Virtual-Platform-Presubmit-Ready: Roger Chang <rogerycchang@google.com>
    Perf-Verified: kokoro <noreply+kokoro@google.com>
    Gem5-Virtual-Platform-Verified: kokoro <noreply+kokoro@google.com>

--- src/arch/riscv/faults.hh
+++ src/arch/riscv/faults.hh
@@ -93,7 +93,7 @@ enum ExceptionCode : uint64_t
     INT_EXT_USER = 8,
     INT_EXT_SUPER = 9,
     INT_EXT_MACHINE = 11,
-    NumInterruptTypes = 256,
+    NumInterruptTypes,
     // INT_NMI does not exist in the spec, it's a modeling artifact for NMI. We
     // intentionally set it to be NumInterruptTypes so it can never conflict
     // with any real INT_NUM in used.
